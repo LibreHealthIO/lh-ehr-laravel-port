72a206daf8ea67950700163df698211914d47300
Terry Hill <teryhill@yahoo.com>
2018-05-12 19:18:58 -0400

    Update PatientDataFactory.php

--- database/factories/PatientDataFactory.php
+++ database/factories/PatientDataFactory.php
@@ -18,7 +18,7 @@ $factory->define(App\PatientData::class, function (Faker\Generator $faker) {
     $industry = array('Law Firm', 'Engineering Firm', 'Construction Firm', 'College'); //Add if any new industry is added in UI
     return [
         'pid' => $faker->unique()->randomNumber($nbDigits = 4, $strict = false),
-        'title' => $faker->title($gender = null|'mail'|'female'),
+        'title' => $faker->title($gender = null|'male'|'female'),
         'occupation' => $occupation[array_rand($occupation, 1)],
 	'industry' => $industry[array_rand($industry, 1)],
 	'addressId' => factory(App\Address::class)->create()->id,
